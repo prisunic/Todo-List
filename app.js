cd456f2e1786b2a4723c0ca1c2d5d4e7359ff5f4
Manthan Laad <laad.manthan@gmail.com>
2020-11-29 14:45:17 +0530

    User input todo item and empty field validation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
+todoInput.value = "";
 // Event Listeners
 todoButton.addEventListener("click", print);
 
@@ -12,28 +13,36 @@ function print(event) {
   event.preventDefault();
 
   // Todo div
-  const todoDiv = document.querySelector(".todo-div");
+  const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
 
   // Create li
   const newTodo = document.createElement("li");
-  newTodo.innerText = "added";
-  newTodo.classList.add("new-todo");
-  todoDiv.appendChild(newTodo);
+  if (todoInput.value == "") {
+    alert("Task cannot be empty");
+    console.warn("Please enter a task");
+  } else {
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("new-todo");
+    todoDiv.appendChild(newTodo);
 
-  // Confirm Button
-  const confirmButton = document.createElement("button");
-  confirmButton.innerHTML = `<i class="fas fa-check"></i>`;
-  confirmButton.classList.add("confirm-btn");
-  todoDiv.appendChild(confirmButton);
+    // Confirm Button
+    const confirmButton = document.createElement("button");
+    confirmButton.innerHTML = `<i class="fas fa-check"></i>`;
+    confirmButton.classList.add("confirm-btn");
+    todoDiv.appendChild(confirmButton);
 
-  // Delete Button
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
-  deleteButton.classList.add();
-  deleteButton.classList.add("confirm-btn");
-  todoDiv.appendChild(deleteButton);
+    // Delete Button
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    deleteButton.classList.add();
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(deleteButton);
 
-  // Adding todoDiv to list
-  todoList.appendChild(todoDiv);
+    // Adding todoDiv to list
+    todoList.appendChild(todoDiv);
+
+    // Clear previous Todo input
+    todoInput.value = "";
+  }
 }
